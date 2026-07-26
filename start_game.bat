3657565b3ebc1f9dc8c95409746c7acfb59eee16
vacryleaf <harryhjh.ycytop@qq.com>
2026-07-26 21:54:01 +0800

    配置

--- start_game.bat
+++ start_game.bat
@@ -4,13 +4,7 @@ setlocal
 set "ROOT=%~dp0"
 set "PROJECT=%ROOT%GameProject"
 
-set "GODOT_471=C:\Program Files (x86)\Godot\Godot_v4.7.1\Godot_v4.7.1-stable_win64.exe"
 set "GODOT_DEFAULT=C:\Program Files (x86)\Godot\godot.exe"
-
-if exist "%GODOT_471%" (
-    start "" "%GODOT_471%" --path "%PROJECT%"
-    exit /b 0
-)
 
 if exist "%GODOT_DEFAULT%" (
     start "" "%GODOT_DEFAULT%" --path "%PROJECT%"
@@ -25,7 +19,6 @@ if %ERRORLEVEL% EQU 0 (
 
 echo Godot executable was not found.
 echo Checked:
-echo   %GODOT_471%
 echo   %GODOT_DEFAULT%
 echo   PATH: godot.exe
 echo.
